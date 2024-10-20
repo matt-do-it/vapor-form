af85bcca29837f34b2bc35e8f47944335d51daa6
Matt Herold <mattherold@icloud.com>
2024-10-20 05:25:10 +0200

    Added missing file.

---
+++ Sources/App/Content/ContactForm.swift
@@ -0,0 +1,16 @@
+import Vapor
+
+struct ContactFormRequest: Content {
+    var name: String
+    var email: String
+    var message: String
+}
+
+extension ContactFormRequest: Validatable {
+    static func validations(_ validations: inout Validations) {
+        validations.add("name", as: String.self, is: !.empty)
+        validations.add("email", as: String.self, is: !.empty)
+        validations.add("email", as: String.self, is: .email)
+        validations.add("message", as: String.self, is: !.empty)
+    }
+}
